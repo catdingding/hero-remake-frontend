15dd9cb6d4402629ca4dee330490966395661c8b
catdingding <tigerrayson@gmail.com>
2020-08-13 20:14:43 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import ViewUI from "view-design";
+import locale from "view-design/dist/locale/zh-TW";
+import "view-design/dist/styles/iview.css";
 
-Vue.config.productionTip = false
+Vue.use(VueAxios, axios);
+Vue.use(ViewUI, { locale });
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
