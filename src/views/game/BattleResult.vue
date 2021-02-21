b5f3c35d75368be48c80c2453fad251b1094ad99
catdingding <tigerrayson@gmail.com>
2021-02-21 20:56:31 +0800

    update battle result display

--- src/views/game/BattleResult.vue
+++ src/views/game/BattleResult.vue
@@ -5,7 +5,7 @@
         <div>攻擊方</div>
         <div>防禦方</div>
       </div>
-      <RecycleScroller class="scroller" :items="filtered_logs" key-field="index" :item-size="110">
+      <RecycleScroller class="scroller" :items="filtered_logs" key-field="index" :item-size="125">
         <template v-slot="{ item }">
           <div class="round">
             <div class="attackers">
@@ -19,11 +19,8 @@
                 </div>
               </div>
               <div class="actions">
-                <div
-                  v-for="(action, action_index) in item.actions.filter((x) => x.team === 'attacker')"
-                  :key="action_index"
-                >
-                  {{ action.message }}
+                <div v-for="(action, action_index) in item.actions" :key="action_index" class="action">
+                  <span v-if="action.team === 'attacker'">{{ action.message }}</span>
                 </div>
               </div>
             </div>
@@ -38,11 +35,8 @@
                 </div>
               </div>
               <div class="actions">
-                <div
-                  v-for="(action, action_index) in item.actions.filter((x) => x.team === 'defender')"
-                  :key="action_index"
-                >
-                  <div>{{ action.message }}</div>
+                <div v-for="(action, action_index) in item.actions" :key="action_index" class="action">
+                  <span v-if="action.team === 'defender'">{{ action.message }}</span>
                 </div>
               </div>
             </div>
@@ -56,6 +50,9 @@
           獲得了 {{ battle_result.proficiency }} 熟練<br />
           獲得了 {{ battle_result.exp }} 經驗值<br />
           <span v-if="loots">獲得了 {{ loots }}<br /></span>
+        </div>
+        <div v-for="(message, index) in battle_result.messages" :key="index">
+          {{ message }}
         </div>
         <router-link to="./"><el-button type="primary" size="medium">返回主頁</el-button></router-link>
       </div>
@@ -80,7 +77,7 @@
         return this.battle_result.logs.filter((x) => x.actions.length !== 0);
       },
       loots() {
-        return this.battle_result.loots.map((x) => x.name + x.number).join("、");
+        return this.battle_result.loots.map((x) => x.name + "×" + x.number).join("、");
       },
     },
     components: { BattleCharaStatus },
@@ -90,9 +87,9 @@
 <style lang="less" scoped>
   .round {
     display: flex;
-    margin-bottom: 10px;
     padding: 10px;
     background-color: #fff;
+    border-bottom: 1px solid #999;
 
     & > .defenders {
       flex-direction: row-reverse;
@@ -107,6 +104,10 @@
     .actions {
       flex: 1;
       text-align: center;
+
+      .action {
+        height: 24px;
+      }
     }
   }
 
