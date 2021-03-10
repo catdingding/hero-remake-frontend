68f85a577b294ac4b7a0e90ea0df9c802be70ab0
catdingding <tigerrayson@gmail.com>
2021-03-11 00:32:10 +0800

    display inn room cost

--- src/views/game/Inn.vue
+++ src/views/game/Inn.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-button type="primary" @click="sleep_inn('room')">住房間(花費金錢，健康度回滿)</el-button>
+    <el-button type="primary" @click="sleep_inn('room')">住房間(花費{{ room_cost }}金錢，健康度回滿)</el-button>
     <el-button type="primary" @click="sleep_inn('stable')">住馬廄(無花費，健康度恢復至50)</el-button>
   </div>
 </template>
@@ -12,7 +12,12 @@
     data() {
       return {};
     },
-    computed: { ...mapState("chara", ["chara_attributes"]) },
+    computed: {
+      ...mapState("chara", ["chara_attributes"]),
+      room_cost() {
+        return this.chara_attributes.reduce((acc, curr) => acc + curr["value"], 0) ** 2;
+      },
+    },
     methods: { ...mapActions("town", ["sleep_inn"]) },
     mounted() {
       this.$store.dispatch("chara/get_chara_profile", { omit: "", fields: "attributes" });
