4227ea9dc415ce88012a339847ceff531ee196a0
catdingding <tigerrayson@gmail.com>
2022-01-25 19:39:47 +0800

    標示競技場佔領小時數

--- src/views/game/Arena.vue
+++ src/views/game/Arena.vue
@@ -4,7 +4,8 @@
       佔領可獲得競技場點數*5，每佔領6分鐘可獲得競技場點數*1(被趕下臺時發放)<br />
       通用競技場獎勵翻倍<br />
       平手情況下，若挑戰者的血量剩餘比例較高也判為挑戰成功<br />
-      每人同時只能佔領一座競技場
+      每人同時只能佔領一座競技場<br />
+      每佔領一小時，能力下降1%，至多下降90%
     </div>
     <div>
       <el-table :data="arenas">
@@ -20,7 +21,8 @@
         </el-table-column>
         <el-table-column label="佔領時間" align="center">
           <template slot-scope="scope">
-            {{ scope.row.occupied_at | datetime_display }}
+            <span>{{ scope.row.occupied_at | datetime_display }}</span>
+            <span v-show="scope.row.occupied_at">({{ compute_hours_passed(scope.row.occupied_at) }}小時前)</span>
           </template>
         </el-table-column>
         <el-table-column label="挑戰" align="center">
@@ -36,7 +38,7 @@
 </template>
 
 <script>
-  import { mapState, mapActions } from "vuex";
+  import { mapState, mapActions, mapGetters } from "vuex";
   import CharaLink from "@/components/CharaLink";
 
   export default {
@@ -45,10 +47,15 @@
       return {};
     },
     computed: {
+      ...mapGetters(["get_client_time"]),
       ...mapState("battle", ["arenas"]),
     },
     methods: {
       ...mapActions("battle", ["get_arenas", "arena_fight"]),
+      compute_hours_passed(time) {
+        var ms = this.get_client_time() - Date.parse(time);
+        return Math.floor(ms / 1000 / 3600);
+      },
     },
     mounted() {
       this.get_arenas();
