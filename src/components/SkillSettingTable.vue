08c439985825526ed425cab7a465faa860664c42
catdingding <tigerrayson@gmail.com>
2022-02-16 23:46:17 +0800

    refactor SkillSettingTable

--- src/components/SkillSettingTable.vue
+++ src/components/SkillSettingTable.vue
@@ -1,19 +1,13 @@
 <template>
-  <table>
-    <tr>
-      <th>優先序</th>
-      <th>技能</th>
-      <th>HP(%)&lt;=</th>
-      <th>MP(%)&lt;=</th>
-      <th>防禦方HP(%)&lt;=</th>
-      <th>防禦方MP(%)&lt;=</th>
-      <th>增加</th>
-      <th>刪除</th>
-    </tr>
-    <tr v-for="(setting, index) in settings" :key="index">
-      <td>{{ index }}</td>
-      <td>
-        <el-select v-model="setting.skill" style="width:250px">
+  <el-table :data="settings" table-layout="auto">
+    <el-table-column align="center" label="優先序">
+      <template v-slot="scope">
+        {{ scope.$index }}
+      </template>
+    </el-table-column>
+    <el-table-column align="center" label="技能">
+      <template v-slot="scope">
+        <el-select v-model="scope.row.skill" style="width:250px">
           <el-option
             v-for="(skill, index) in available_skills"
             :key="index"
@@ -21,97 +15,108 @@
             :label="skill.name + `(mp:${skill.mp_cost},威力:${skill.power},發動率:${skill.rate}%)`"
           ></el-option>
         </el-select>
-      </td>
-      <td>
+      </template>
+    </el-table-column>
+    <el-table-column align="center" label="HP(%)<=">
+      <template v-slot="scope">
         <el-input-number
           :min="0"
           :max="100"
-          v-model="setting.hp_percentage"
+          v-model="scope.row.hp_percentage"
           :formatter="(value) => `${value}%`"
           :parser="(value) => value.replace('%', '')"
         >
         </el-input-number>
-      </td>
-      <td>
+      </template>
+    </el-table-column>
+    <el-table-column align="center" label="MP(%)<=">
+      <template v-slot="scope">
         <el-input-number
           :min="0"
           :max="100"
-          v-model="setting.mp_percentage"
+          v-model="scope.row.mp_percentage"
           :formatter="(value) => `${value}%`"
           :parser="(value) => value.replace('%', '')"
         >
         </el-input-number>
-      </td>
-
-      <td>
+      </template>
+    </el-table-column>
+    <el-table-column align="center" label="防禦方HP(%)<=">
+      <template v-slot="scope">
         <el-input-number
           :min="0"
           :max="100"
-          v-model="setting.defender_hp_percentage"
+          v-model="scope.row.defender_hp_percentage"
           :formatter="(value) => `${value}%`"
           :parser="(value) => value.replace('%', '')"
         >
         </el-input-number>
-      </td>
-      <td>
+      </template>
+    </el-table-column>
+    <el-table-column align="center" label="防禦方MP(%)<=">
+      <template v-slot="scope">
         <el-input-number
           :min="0"
           :max="100"
-          v-model="setting.defender_mp_percentage"
+          v-model="scope.row.defender_mp_percentage"
           :formatter="(value) => `${value}%`"
           :parser="(value) => value.replace('%', '')"
         >
         </el-input-number>
-      </td>
-      <td><el-button type="success" @click="add_setting(index)">增加</el-button></td>
-      <td><el-button type="danger" @click="remove_setting(index)">刪除</el-button></td>
-    </tr>
-  </table>
+      </template>
+    </el-table-column>
+    <el-table-column align="center" label="增加">
+      <template v-slot="scope">
+        <el-button type="success" @click="add_setting(scope.$index)">增加</el-button>
+      </template>
+    </el-table-column>
+    <el-table-column align="center" label="刪除">
+      <template v-slot="scope">
+        <el-button type="danger" @click="remove_setting(scope.$index)">刪除</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
 
-<script>
-  export default {
-    name: "SkillSettingTable",
-    data() {
-      return {};
+<script setup>
+  import { defineProps, defineEmits, computed, watch } from "vue";
+  const props = defineProps({
+    available_skills: { type: Array, default: () => [] },
+    modelValue: { type: Array, default: () => [] },
+  });
+  const emit = defineEmits(["update:modelValue"]);
+
+  const settings = computed({
+    set(value) {
+      emit("update:modelValue", value);
     },
-    props: { available_skills: { type: Array, default: () => [] }, modelValue: { type: Array, default: () => [] } },
-    computed: {
-      settings: {
-        set(value) {
-          this.$emit("input", value);
-        },
-        get() {
-          return this.modelValue;
-        },
-      },
+    get() {
+      return props.modelValue;
     },
-    methods: {
-      add_setting(index) {
-        this.settings.splice(index + 1, 0, {
-          skill: null,
-          hp_percentage: 100,
-          mp_percentage: 100,
-          defender_hp_percentage: 100,
-          defender_mp_percentage: 100,
-        });
-      },
-      remove_setting(index) {
-        this.settings.splice(index, 1);
-      },
-    },
-    watch: {
-      settings: {
-        immediate: true,
-        handler() {
-          if (this.settings.length === 0) {
-            this.add_setting(-1);
-          }
-        },
-      },
-    },
-    mounted() {},
+  });
+
+  const add_setting = (index) => {
+    settings.value.splice(index + 1, 0, {
+      skill: null,
+      hp_percentage: 100,
+      mp_percentage: 100,
+      defender_hp_percentage: 100,
+      defender_mp_percentage: 100,
+    });
   };
+  const remove_setting = (index) => {
+    settings.value.splice(index, 1);
+  };
+
+  watch(
+    settings,
+    () => {
+      if (settings.value.length === 0) {
+        add_setting(-1);
+      }
+    },
+    { deep: true }
+  );
 </script>
 
 <style lang="less" scoped></style>
